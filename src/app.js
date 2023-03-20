844a800f044f011b45395a1b7e8adfe8676e7cef
Mohit7524 <mohit7524938692@gmail.com>
2023-03-20 14:46:35 +0530

    commit

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+//Connection Creation and create a new Database
+
 const mongoose= require("mongoose");
 mongoose.connect("mongodb://localhost:27017/mohitVirtue")
 .then( () => console.log("connection done successfully......."))
